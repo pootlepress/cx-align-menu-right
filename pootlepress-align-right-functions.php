3a7d1e065a21476d8392eb2358bcb18955d6628d
Alan Ng <jenfoong@gmail.com>
2014-07-16 16:02:05 +0800

    Add auto update

--- pootlepress-align-right-functions.php
+++ pootlepress-align-right-functions.php
@@ -50,4 +50,3 @@ function fixStickyMobile(){
 	</script>
 <?php
 }
-?>
--- pootlepress-align-right.php
+++ pootlepress-align-right.php
@@ -11,7 +11,7 @@ License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2
 /*  Copyright 2014  Pootlepress  (email : jamie@pootlepress.co.uk)
 
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
+    it under the terms of the GNU General Public License, version 2, as
     published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
@@ -23,13 +23,26 @@ License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-	
+
     if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 	require_once( 'pootlepress-align-right-functions.php' );
 	require_once( 'classes/class-pootlepress-align-right.php' );
+    require_once( 'classes/class-pootlepress-updater.php');
 
     $GLOBALS['pootlepress_align_right'] = new Pootlepress_Align_Right( __FILE__ );
     $GLOBALS['pootlepress_align_right']->version = '1.0.2';
-	
+
+add_action('init', 'pp_ar_updater');
+function pp_ar_updater()
+{
+    if (!function_exists('get_plugin_data')) {
+        include(ABSPATH . 'wp-admin/includes/plugin.php');
+    }
+    $data = get_plugin_data(__FILE__);
+    $wptuts_plugin_current_version = $data['Version'];
+    $wptuts_plugin_remote_path = 'http://jamiemarsland.staging.wpengine.com/updater';
+    $wptuts_plugin_slug = plugin_basename(__FILE__);
+    new Pootlepress_Updater ($wptuts_plugin_current_version, $wptuts_plugin_remote_path, $wptuts_plugin_slug);
+}
 ?>
