5c74ab79672a2a70e1cb54d5470857e670e27141
Shramee Srivastav <shramee.srivastav@gmail.com>
2016-10-14 20:06:13 +0530

    Ready for 1.2.1

--- pootlepress-align-right.php
+++ pootlepress-align-right.php
@@ -3,7 +3,7 @@
 Plugin Name: Canvas Extension - Align Menu Right
 Plugin URI: http://pootlepress.com/canvas-extensions/
 Description: An extension for WooThemes Canvas that aligns the menu to the right of the logo.
-Version: 1.2
+Version: 1.2.1
 Author: PootlePress
 Author URI: http://pootlepress.com/
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -32,19 +32,4 @@ License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2
     require_once( 'classes/class-pootlepress-updater.php');
 
     $GLOBALS['pootlepress_align_right'] = new Pootlepress_Align_Right( __FILE__ );
-    $GLOBALS['pootlepress_align_right']->version = '1.2';
-
-add_action('init', 'pp_ar_updater');
-function pp_ar_updater()
-{
-    if (!function_exists('get_plugin_data')) {
-        include(ABSPATH . 'wp-admin/includes/plugin.php');
-    }
-    $data = get_plugin_data(__FILE__);
-    $wptuts_plugin_current_version = $data['Version'];
-    $wptuts_plugin_remote_path = 'http://www.pootlepress.com/?updater=1';
-//    $wptuts_plugin_remote_path = 'http://pootle.localhost/wordpress/?updater=1';
-    $wptuts_plugin_slug = plugin_basename(__FILE__);
-    new Pootlepress_Updater ($wptuts_plugin_current_version, $wptuts_plugin_remote_path, $wptuts_plugin_slug);
-}
-?>
+    $GLOBALS['pootlepress_align_right']->version = '1.2.1';
